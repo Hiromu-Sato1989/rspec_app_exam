8907a970182834e3c1fba2e848d4f08a5d2112c8
Hiromu Sato <a.string.d@gmail.com>
2021-02-03 10:05:34 +0900

    Fix: 別タブ遷移後のテストが可能になるよう修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -17,10 +17,12 @@ RSpec.describe 'Task', type: :system do
 
       it 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
         # FIXME: テストが失敗するので修正してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
+        # project = FactoryBot.create(:project)
+        # task = FactoryBot.create(:task, project_id: project.id)
         visit project_path(project)
         click_link 'View Todos'
+        handle = page.driver.browser.window_handles.last
+        page.driver.browser.switch_to.window(handle)
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
         expect(current_path).to eq project_tasks_path(project)
